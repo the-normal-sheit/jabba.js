40a7c1165c96b4e9d605fe5d143806ad5bf24918
Monky AI - BIA projects or abandonware <jysemoe@gmail.com>
2026-01-03 23:41:31 -0600

    jabba data

---
+++ data.js
@@ -0,0 +1,83 @@
+[
+`
+You know what? I admitted it. I'm done with you all. These haters won't leave me alone and you guys are next to harass me for harassing the most subscribed Brony fan "James" that we wrongly targeted. They ruined my entire career due to their clueless drama. They never accepted my apology like little midgets.
+
+I am behind these accounts. I created @SUPER GMK // REBIRTH​​​​ as a revived account to attack and embarrass myself, unlike Rick. I created Random ICQ Bangers a.k.a OPP Tweets as an Old Phone Preservation backup account to get away from my identity coming from ‪@romphonix‬​​​​, I was also the one who noted the incidents.
+
+My real name is Dani€lius Paulavi€ius. The post you can see is my real face that I never revealed if I was allowed to do that. I was the same person behind these accounts named JABBA RANTS EVERYTHING!, IUS, Discord World, itzcrazynil minecraft - those are being deleted. I live in Lithuania, not Taiwan. My actual age is 19 and my religion is Catholic. I finished my school on May 27th, 2025.
+
+My apologies are trash, so guess what? I am the same person who is doing this. Two accounts to be exact. And do you want to know how many accounts I have for the rest of my YouTube career? I have 50 alternative accounts. I created 50 FUCKING Google accounts and it was about me to attack myself that happened in 2019 to 2020. You would be shocked how much I have.
+
+Everything I have done on the internet, I don't think that's an option for writing every single thing I have done. That was a lot of things I have done since March 2017 to current, and I feel like people don't want to listen to me.
+
+James and Karon, this is all your fault. You literally killed my channel. I hope you two get deplatformized for the rest of your lives.
+
+And who's watching this post...
+Wow, just fucking WOW! This is why we can't have nice things! And you know what? I'm out of here. Have fun here in my dusty YouTube channel because I am not coming back. I'm sick and tired of you going around like a fucking bee swarm all the time, I've had enough of your stupidity, I've REALLY had enough of being treated as an object, so what guys are you fucking thinking? You think you can call me Jabba as a word that I was always called because I am fucking morbidly obese?
+
+You shouldn't behave this way, I tried to teach you a lesson and NOW THIS?! What the fuck is wrong with you? This is why you bronies who hate me for harassing the most successful YouTubers in the world, you can say that I am cancelled or approved, that's what the CEO of Twitter decides. Why can't you behave like a normal person when I told you not to do it?
+
+You really have to shake your heads and drop it, you ambassadors. I am only a person on the internet who likes to do stuff, but racism and swearing is my passion and I feel like doing it to ruin people's reputations. I ruined three people's reputations as well (SgtPepperArc360, Karonzilla and Saturned05/Joopter) and they never came back how I threw Samsung refrigerators at them. This isn't a crashout, this is a goodbye message.
+
+I feel like I don't want to interact with Elliott Enantiornithine anymore, the way he treated one of them like that and I am cutting ties with him, PERMANENTLY. You heard that? I am genuinely cutting ties with him. He is not going to get another chance from his fucking head.
+
+Elliott (a.k.a. Brandon Bratol), if you're watching this post, I'm sorry, but I have to cut ties with you because you are using it as a hidden weapon to attack someone and manipulate them. DO YOU THINK THIS IS A GOOD IDEA TO HARASS THE INNOCENT PEOPLE LIKE THAT, HUH?! WHY ARE YOU GOING AROUND LIKE A SQUIRREL CHEWING ACORN TRYING TO BLAME THE ALLEGATIONS OUT OF INNOCENT PEOPLE?! This is not how you attack James in every way, shape or form, you may misheard his commands and you did it for yourself. DO YOU HEAR ME?! That's it. We're fucking done for good. We're done. Goodbye forever. This is not an excuse for you to do that. You can send death threats all you want, whenever you want, because you will always remain stupid, toxic and incinerated, and finally, you will always be a predator for your whole life after what you did on November 30th, 2025. You are a pedophile. You are nothing but a manipulative piece of shit trying to debunk the allegations out of someone that they didn't. How about you go get a fucking job and focus on something useful with your life instead of harassing people like a fucking jackass? No one wants to be your friend, you made them disgusted and felt like you are one of the individual problematic people on the internet. If I were you, I hope you will be exposed to the news. I don't even know what AU's are for you and I do not want to hear it. End of story.
+If you try to come back after my break is over, I will block you, mass report you and report you to the police. I will never accept your apology for things you have done and always will be. You will be in jail and you will deserve how you acted stupid for years prior.
+
+If things don't change, you can hate me all you want. That's how your behavior is. Nobody cares if your behavior is too rough, then go away and never come back. Go somewhere else to communicate and develop. If not, grab a sharp knife and keys. I hope you go to hell for things you have done to me.
+
+The evidence of myself is already revealed in one of my community posts. Speaking of evidence, the links of the community post are located where we faced the situation from James Elife. You can expose me if you want to and you can do it. I will not watch your video, I will not see what you are doing, I don't fucking care. You can name the internet the bad and that's it.
+
+Do you want to know what my old enemies are? Here there are:
+‪@siob_x86‬​​​​​
+‪@xXNikoFromOneShotXx‬​​​​​
+@kern3l
+‪@ProjectPoyo‬​​​​​
+‪@ardatherandomguy1608‬​​​​​
+My arch-nemesis Karon
+...and more I can't remember. I may have forgotten the names after I moved on. You can still contact them if they still know what I did, but I don't feel like interacting with them because they got me in trouble a year ago.
+
+When I take a break from the internet, you can still wish me a happy birthday (January 8th, 2026, to be at least). I will never reply, heart, like and see your comments because I'm already out. The actual duration of no internet, I will come back in 2028, the SAME day of the month and see if everything changes or not.
+If the decisions change if I was beyond redemption against my old enemies and my new friends that I made damages with, I will abandon this channel forever like how ‪@SawtoothWaves‬​​​​​ and ‪@The25KAPRTV‬​​​​​ did by wiping the entire channel but leaving final words. I would be gone into obscurity. I would already have made an account that is very secretive such as my musical compositions, but you will never know.
+
+Why are you not helping me improve? Why are you not helping me avoid the situations between them? This is a waste of time. Why can't you just help me? I don't know what to use such as a new technology while I still know where it was and why can't you teach me how to do it? You are being a helpless, indigenous piece of shit who doesn't know how to read and write, you are an illiterate. I'd rather kiss a cactus than listen to you if you can't afford to help me like a foolish moron who has a lot of work to do and you are a puny drug addict, injected with morphine like Thorax has. Why can't YOU just help me develop?! IF YOU KEEP MAKING ME MAD AGAIN AND AGAIN, I HAVE NO CHOICE BUT END YOUR FRIENDSHIP ONCE AND FOR ALL!!!
+
+Your friends and the community are full of internet trolls and narcissistic fools who don't know how to behave. But if you want to behave, get a fucking life or get a new life if you already have or go rot in hell. I am not going to say this again. If I see one of your disturbing comments saying that I need to get a job, I will smash my fucking phone into millions of pieces and shred it with a blender. Do you know I have level 3 autism after I was 7 years old in 2013? Of course you don't.
+
+My Little Pony is one of my childhood series that I watched in 2015, so don't expect ruining that one... but you did it anyway. You ruined my childhood, you FUCKING ruined my childhood, all because of one person that I got manipulated by this retard and you're the ones who made me feel bad. You think I am dumb? Why am I so terrible? Do I need to write more than you guys can know what happened? I don't get it!
+
+Can you please forgive me? I am very sorry for everything. I promise I won't do that again. Give me another chance and I will behave like a normal person. I'm taking a break from the ENTIRE internet for 3 years. And not only that, I will unplug my internet connection, live with it and read some books that I have never read in my life.
+
+If you say that my apology is denied, you really need some serious help. I hope you choke a curtain, stun yourself with an electric flyswatter, eat Play-Doh clay, put colored crayons on your nose, step on a Lego brick and swallow the pieces from Monopoly if that's how you plan to do it. Are you all still waiting for my death because I am a lowlife degenerate and terrible person who did bad things on the internet? I don't need all of you standing like Toads from Super Mario 64 or some shit you are from the internet. This YouTube channel after I came back to the community, I made you guys happy, but no, you betrayed my fucking trust.
+
+From now on, My Little Pony Sora AI Series is now cancelled and there will be no uploads. You have to watch something else. I am leaving all the communities and letting them believe to forgive me if I was in a bad shape. My mental health is risky for me, if you prove my point.
+
+I want everyone to be happy. I want to go back to 2015, the year that has no drama. I hope everyone will befriend me, make me successful by telling some large YouTubers, not spitefulness. Goodbye.
+`,
+`
+ItzUltraScout's era is coming to end. All of my career that I used to work on are gone forever due to my parents forced me to delete my main YouTube channel because of my face and my actual information being posted in public that I used to introduce myself, and I need to remove them all such as old YouTube channels I owned, had to be privated. Some websites, for example Fandom, a random subdomain of the wiki has my information about me when I was stupid back then and I can't completely remove them because of the automated filter said that I am vandalizing the article, tried to make it blank and it's very limited. How pathetic. Can't you people can be smart for fuck's sakes? Get yourselves a life you little bastards, you know that thing and please how in early days of Fandom has changed hereafter. I swear to god, will you ever remove my article entirely otherwise my mom's sister will have to see my article under my deadname on the wiki, read it again and tell me that I didn't remove it until the end of the year? My information under this article is a serious concern and you will have to remove this bullshit or else I will send the goddamn government papers against you to sue your wiki! Would you get me in trouble for not doing that? I would call the police upon you and he will tell me where you live (if you live under the sea with stupid rocks, seahorse, kelps, jellyfish and corals, I can prove you live in the aquarium and the rest of the police can still see what you are doing) which I can almost send a mysterious pizza with sweet gunpowder where the delivery boy can deliver you, would it be a big problem for all of that? Then do it already and also learn what handling criticism technique works. In fact, I just found you doing things that you don't really care about removing a single article if the wiki is not a rocket science when it was requested by user for privacy reasons. Do you know what privacy reasons mean? It's the same thing as doxxing because you don't fucking know how to speak proper English because I think you are Scottish coward who twists your tongue or something shit you took from. Please learn how doxxing word means and why you should avoid that, and please learn how to complete remove the article from the wiki. It's pretty simple. I DID ACTUALLY REQUEST YOU TO REMOVE MY ARTICLE BUT YOU DIDN'T. For your mistakes, I am 18 year old man and I want my real name to be removed or else I will report you to the authorities to pay you a fine for refusal. So remember that guys!
+
+In early days of December 2024, I posted the link of my website on my community tab, admitting my wrongdoings and refrain from doing that again. My parents did the same thing as they wanted me to stop being a YouTuber for all the time and forced me to become a casual person doing casual things in reality such as gaming, watching movies, doing good things instead of doing the same thing that contains my face and ridiculing me.
+
+Here are a few screenshots I just took from my website and they are written by me. Since I don't have anything to say, would it be possible to improve myself into attacking criminals and some shit for what he has did? Well, no. That would be the same thing as I didn't improve, so I want to forget everything for what I did and let it repair itself. Hasta la vista, my friends. I hope you go without me.
+`,
+`
+Oh well, do I have to explain all over again? Look down for the evidence I have made and I don't want to waste
+my time for writing the same things I have did and I do not want to talk about anyone who cut ties with me or
+I'm relating to anything. And who are watching this, I still HAD it. I still HAD the chance to talk about it. I still
+HAVE the energy to talk about my past because without my past, I could not remember what happened then
+like in 2021 or some shit I have did without them before they get around me a lot which is totally bad, and I
+can't actually remember the year of 2016 for things I have did because I only know what really happened in
+2017 before I abandoned my YouTube channel for 6 years before I brought my old account back in 2023. That
+was super unfair. The fair is do I actually have to show you what am I doing? Match three games are
+surprisingly awesome, don't you? Bejeweled 3 is only the game I need and I was highly obsessed with
+Bejeweled series and made me a lot of improvement. And so let's not talk about the gem-matching game made
+by PopCap Games or GameHouse, should I say. Why would you guys have to be like that? Go back to 2018 and
+act like this now, wouldn't you change that as a 12 year old kid who talks nonsense and do nothing at all? Why
+do people hate my past? There is nothing wrong about my past unless if you manipulated my friends. I just
+really wanted to have fun and that's all. There was only one user on Discord who saw me in the group chat or
+inside of normal Discord server who still knows me that I was doing back in 2015 with my forbidden username
+that I have used for years making logo editing content and still obsessed with "class key chew poe" (or Splaat,
+if you know what I mean) until I finally got rid of that thing.
+`
+]
